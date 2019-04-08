03dd4d66ba24ccf8ce1e12c01ffeb6be63046e78
Samir Jasarat <samirjasarat@gmail.com>
2019-04-08 15:29:29 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars=["snickers","hundred grand", "kitkat", "skittles"]
+
+console.log(chocolateBars)
+
+function addElementToBeginningOfArray(array, element){array.unshift(element)}
+
+addElementToBeginningOfArray([1],"too");
+
+console.log(addElementToBeginningOfArray)
